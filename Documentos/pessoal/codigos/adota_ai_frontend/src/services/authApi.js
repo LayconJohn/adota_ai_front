e75483e23e5dafdf67fc66ad4469f4e4109fd31a
layconjohn <layconjohn@gmail.com>
2023-07-25 00:57:19 -0300

    refactor(auth): Update endpoint auth

--- Documentos/pessoal/codigos/adota_ai_frontend/src/services/authApi.js
+++ Documentos/pessoal/codigos/adota_ai_frontend/src/services/authApi.js
@@ -1,12 +1,12 @@
 import api from "./api";
 
 async function signUp(body) {
-    const promise = await api.post('/signup', body);
+    const promise = await api.post('/user', body);
     return promise;
 }
 
 async function signIn(body) {
-    const promise = await api.post('/signin', body);
+    const promise = await api.post('/login', body);
     return promise;
 }
 
